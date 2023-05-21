963e3f67708c270c048db65169b2a21480c8636e
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-05-21 08:25:35 -0600

    search history added, picture icons included

--- assets/script.js
+++ assets/script.js
@@ -9,44 +9,110 @@ var condition = document.getElementById("condition");
 var feelsLike = document.getElementById("feels-like");
 var timeDisplayEl = document.getElementById("time");
 
+
+
+
+
 function displayTime() {
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.textContent = rightNow;
-  }
-  
-async function  checkWeather(city){
-  const response = await fetch(weatherApiUrl + city + `&appid=${apiKey}`);
-  const data = await response.json();
-  console.log(data);  
+}
+
+async function checkWeather(city) {
+    const response = await fetch(weatherApiUrl + city + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
 
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F"; 
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "mph";
-    
-  if (data.weather[0].main === "Clouds") {
+    document.querySelector(".visibility").innerHTML = data.visibility + " miles";
+    document.querySelector(".pressure").innerHTML = data.main.pressure + " hPa";
+    document.getElementById("sun-rise").innerHTML = dayjs.unix(data.sys.sunrise).format('hh:mm a');
+    document.getElementById("sun-set").innerHTML =  dayjs.unix(data.sys.sunset).format('hh:mm a');
+
+    if (data.weather[0].main === "Clouds") {
         weatherIcon.src = "assets/images/cloudy.png";
-        
         condition.innerHTML = "Cloudy";
         feelsLike.innerHTML = "Feels like " + Math.round(data.main.feels_like) + "°F";
-} else if (data.weather[0].main === "Rain") {
-    weatherIcon.src = "assets/images/rainy.png";
-    condition.innerHTML = "Rainy";
-} else if (data.weather[0].main === "Snow") {
-    weatherIcon.src = "assets/images/snowy.png";
-    condition.innerHTML = "Snowy";
-} else if (data.weather[0].main === "Clear") {
-    weatherIcon.src = "assets/images/sunny.png";
-    condition.innerHTML = "Sunny";
-} else if (data.weather[0].main === "Thunderstorm") {
-    weatherIcon.src = "assets/images/thunderstorm.png";
-    condition.innerHTML = "Thunderstorm";
+    } else if (data.weather[0].main === "Rain") {
+        weatherIcon.src = "assets/images/rainy.png";
+        condition.innerHTML = "Rainy";
+    } else if (data.weather[0].main === "Snow") {
+        weatherIcon.src = "assets/images/snowy.png";
+        condition.innerHTML = "Snowy";
+    } else if (data.weather[0].main === "Clear") {
+        weatherIcon.src = "assets/images/sunny.png";
+        condition.innerHTML = "Sunny";
+    } else if (data.weather[0].main === "Thunderstorm") {
+        weatherIcon.src = "assets/images/thunderstorm.png";
+        condition.innerHTML = "Thunderstorm";
+    }
 }
+function searchHistory (city){
+    const searchHistory = document.querySelector('.history-list');
+    var historyList = document.createElement("li");
+    
+        historyList.textContent = city;
+        searchHistory.appendChild(historyList);
+
+        historyList.addEventListener("click", function () {
+        checkWeather(historyList.textContent);
+        forecastDataBase(historyList.textContent);
+    }
+    )
 }
+function forecastDataBase(city) {
+
+    var url = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=' + city + '&appid=' + apiKey;
+    
+    
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            var forecast = data.list;
+            var forecastList = document.querySelector(".forecast-list");
+            var previousDate = null;
+
+            forecastList.innerHTML = "";
+
+            forecast.forEach(function (forecastDay) {
+                var forecastDate = forecastDay.dt_txt.split(" ")[0];
+                var forecastTemp = forecastDay.main.temp;
+                var forecastHumidity = forecastDay.main.humidity;
+                var forecastWind = forecastDay.wind.speed;
+
+                console.log(`Date/Time: ${forecastDate} Temp: ${forecastTemp} Humidity: ${forecastHumidity} Wind: ${forecastWind}`);
+                
+                if (forecastDate !== previousDate) {
+                    var listItem = document.createElement("li");
+
+                    listItem.textContent = 
+                  ` ${forecastDate} 
+                    ${forecastTemp}  
+                    ${forecastHumidity} 
+                    ${forecastWind}`;
+
+                    forecastList.appendChild(listItem);
+                    previousDate = forecastDate;
+
+                    inputEl.value = "";
+                }
+            });
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+}
+
 displayTime();
 setInterval(displayTime, 1000);
 
-buttonEl.addEventListener("click", function(){   
-checkWeather(inputEl.value);
+buttonEl.addEventListener("click", function () {
+    checkWeather(inputEl.value);
+    forecastDataBase(inputEl.value);
+    searchHistory(inputEl.value);
 });
